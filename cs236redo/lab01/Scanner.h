ce26a3580161a624e5282e8d5cab80f2507697a1
nerdburton <gen.warburtonc@gmail.com>
2016-09-23 23:35:41 -0600

    modified scanner.h file

--- cs236redo/lab01/Scanner.h
+++ cs236redo/lab01/Scanner.h
@@ -36,4 +36,60 @@ using namespace std;
 
 class Scanner {
 public:
-	
+	int lineNum;
+	vector<Token> tokenStore;
+
+	// scan in string
+	string import_text(string filename);
+
+	/**
+	 * Main hub
+	 * 
+	 * Reads a character in the string and chooses where to go
+	 *
+	 * For single character tokens (always single character like LEFT_PAREN)
+	 * send it to single character tokenizer
+	 * 
+	 * for multicharacter tokens (IDs, strings and comments)
+	 * send to multicharacter tokenizer
+	 */
+	void tokenizer_hub(string fileText);
+
+	/**
+	 * recursively creates strings for the tokens
+	 */
+	string stringer(string str);
+	string commenter(string str);
+	string block_commenter(string str);
+	string ider(string str);
+
+	void string_tokenizer();
+	void comment_tokenizer();
+	void block_comment_tokenizer();
+	void id_tokenizer();
+	void char_tokenizer();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

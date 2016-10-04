2f0e9edb98b846af76acde1f99accb36f9fe331e
nerdburton <gen.warburtonc@gmail.com>
2016-10-04 16:52:47 -0600

    Scanner.cpp in progress

--- cs236redo/lab01/Scanner.cpp
+++ cs236redo/lab01/Scanner.cpp
@@ -1,11 +1,27 @@
 #include "Scanner.h"
 
 string Scanner::import_text(string filename) {
-
+	char ch;
+	string str;
+	fstream fin(filename.c_str(), fstream::in);
+	while (fin >> noskipws >> ch) {
+		str += ch;
+	}
+	return str;
 }
 
 void Scanner::tokenizer_hub(string fileText) {
+	char ch;
 
+	while (stringStillHasStuff)
+	{
+		ch = firstCharacterOfFileText
+		if(is_char_token(ch)) {
+			charTokenizer(ch)
+			fileText = fileTextTruncated
+		}
+		else if
+	}
 }
 
 bool Scanner::is_char_token(char ch) {
